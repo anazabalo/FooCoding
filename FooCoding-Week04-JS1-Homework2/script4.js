87528f04ae37d0a836b2b47fcaf038f507f8db1d
Ana Zabalo <89654940+anazabalo@users.noreply.github.com>
2021-09-29 12:31:28 +0200

    Add files via upload

---
+++ FooCoding-Week04-JS1-Homework2/script4.js
@@ -0,0 +1,6 @@
+const y = 'one'
+console.log ('the value of the string is a one')
+console.log (y)
+
+const y = 'two'
+console.log ('will be display and error since a const cannot be changed')
---
+++ FooCoding-Week04-JS1-Homework2/script8.js
@@ -0,0 +1,18 @@
+let one = 2
+console.log (one)
+console.log ('the type of my variable is primitive because it is a number, and it is inmutable')
+console.log (typeof one);
+
+let two = 4
+console.log (two)
+
+let three = 6
+console.log (three)
+
+let four = 8
+console.log (four)
+
+two > one && three < four
+
+if (one = two) {
+  console.log ('SAME TYPE');}
