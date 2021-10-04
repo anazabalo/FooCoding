19e0092c3c5cc19332ce5acf71bec1afe9299dae
Ana Zabalo <89654940+anazabalo@users.noreply.github.com>
2021-10-05 00:21:24 +0200

    Delete FooCoding-Week04-JS1-Homework2 directory

--- FooCoding-Week04-JS1-Homework2/script9.js
+++
@@ -1,9 +0,0 @@
-let x = 7
-
-x = x %3
-
-// value of x is 1
-
-console.log ('first check how many times the small number can fit inside the big number')
-console.log ('that it means that shared the number in smaller portions')
-console.log ('the result will be the remining of the proportional division of the big number')
